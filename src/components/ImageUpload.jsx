ff9ebce9e3927668148f292d657ca283b4e21e52
cjmin_n <mymemose@naver.com>
2025-03-12 15:22:35 +0900

    api 통신

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -6,11 +6,13 @@ import "../styles/imageUpload.css";
 function ImageUpload() {
   const [preview1, setPreview1] = useState("");
   const [preview2, setPreview2] = useState("");
+  const [image1, setImage1] = useState("");
+  const [image2, setImage2] = useState("");
   const [name1, setName1] = useState("나");
   const [name2, setName2] = useState("타인");
   const navigate = useNavigate();
 
-  const handleImageChange = (e, setPreview) => {
+  const handleImageChange = (e, setPreview, setImage) => {
     const file = e.target.files[0];
     if (file) {
       const reader = new FileReader();
@@ -18,17 +20,41 @@ function ImageUpload() {
         setPreview(reader.result);
       };
       reader.readAsDataURL(file);
+      setImage(file);
     }
   };
+
+  const uploadImages = async (file1, file2) => {
+    const formData = new FormData();
+    formData.append("image1", file1);
+    if (file2) {
+        formData.append("image2", file2); // 두 번째 파일이 있을 경우 추가
+    }
+
+    try {
+        const response = await fetch("http://127.0.0.1:8000/analyze/", {
+            method: "POST",
+            body: formData, // FormData를 직접 전송
+        });
+
+        const result = await response.json();
+        console.log("서버 응답:", result);
+    } catch (error) {
+        console.error("파일 업로드 중 오류 발생:", error);
+    }
+};
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // 두 이미지가 모두 업로드되었는지 확인
-    if (!preview1 || !preview2) {
+    if (!image1 || !image2) {
       alert("두 개의 이미지를 모두 업로드해주세요!");
       return;
     }
+
+    uploadImages(image1, image2);
 
     // 결과 페이지로 이동하면서 이미지와 이름 데이터를 전달
     navigate("/result", {
@@ -40,6 +66,8 @@ function ImageUpload() {
       },
     });
   };
+
+  
 
   return (
     <div className="upload-container">
@@ -61,7 +89,7 @@ function ImageUpload() {
               )}
               <input
                 type="file"
-                onChange={(e) => handleImageChange(e, setPreview1)}
+                onChange={(e) => handleImageChange(e, setPreview1, setImage1)}
                 className="file-input"
               />
             </label>
@@ -91,7 +119,7 @@ function ImageUpload() {
               )}
               <input
                 type="file"
-                onChange={(e) => handleImageChange(e, setPreview2)}
+                onChange={(e) => handleImageChange(e, setPreview2, setImage2)}
                 className="file-input"
               />
             </label>
